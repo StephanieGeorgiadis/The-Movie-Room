896060008ea9a118e919437cb4c91ce555f53898
Jameskutty Sebastian <jameskutty.sebastian@gmail.com>
2019-12-10 21:03:50 +1030

    updated movies from a list api

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,10 +2,26 @@
 
 // $(".carousel").empty({fullWidth: true});// to make it full width
 
-var id = [14672, 260312, 13852, 11646, 2759, 9555, 9659, 13433, 5257, 236, 13439, 67748, 24266, 26405, 298382, 36040, 256917, 43930, 14064, 9993];
-var initialUrl = "https://api.themoviedb.org/3/movie/";
+// var id = [14672, 260312, 13852, 11646, 2759, 9555, 9659, 13433, 5257, 236, 13439, 67748, 24266, 26405, 298382, 36040, 256917, 43930, 14064, 9993];
+var initialUrl = "https://api.themoviedb.org/3/search/movie?api_key=cc93940bfc0e8ed610b23598baac74fd&query=Australian&include_adult=false&region=Australian";
 var themapi_key = "?api_key=cc93940bfc0e8ed610b23598baac74fd";
 var imgLoadedCount = 0;
+var id =[];
+
+function getMovieIDs(){
+    $.ajax({
+        url: initialUrl,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        for (var x=0; x<response.results.length; x++ ){
+            if(response.results[x].poster_path){
+                id.push(response.results[x].id);
+            }            
+        }
+        loadInitialCarousel(id);
+    })
+}
 
 function loadInitialCarousel() {
     for (var i = 0; i < id.length; i++) {
@@ -13,10 +29,11 @@ function loadInitialCarousel() {
     }
 }
 
-
+var carouselUrl = "https://api.themoviedb.org/3/movie/";
+var themapi_key = "?api_key=cc93940bfc0e8ed610b23598baac74fd";
 
 function loadCarousel(i) {
-    var apiURL = initialUrl + id[i] + themapi_key;
+    var apiURL = carouselUrl + id[i] + themapi_key;
     var hreF = id[i];
 
     $.ajax({
@@ -33,11 +50,12 @@ function loadCarousel(i) {
 
         if (imgLoadedCount == id.length) {
             $(".carousel-item").on("click", function () {
+                
                 if ($(this).attr("class").includes("active")) {
                     console.log(this);
                     var currentMovieId = this.attributes.href.value;
                     console.log(currentMovieId);
-                    movieInfoUrl = initialUrl + currentMovieId + themapi_key;
+                    movieInfoUrl = carouselUrl + currentMovieId + themapi_key;
 
                     $.ajax({
                         url: movieInfoUrl,
@@ -45,15 +63,28 @@ function loadCarousel(i) {
                     }).then(function (response) {
                         console.log(response);
                         $("#movieName").text(response.original_title);
-                        $("#movieImg").attr("src", "https://image.tmdb.org/t/p/w500" + response.backdrop_path);
+                        if(response.backdrop_path){
+                            $("#movieImg").attr("src", "https://image.tmdb.org/t/p/w500" + response.backdrop_path);
+                        }
+                        
                         if(response.tagline !== ""){
                             $("#movieTag").text("Tagline : "+response.tagline);
                         }
                         
                         $("#movieDescription").text("Story: "+response.overview);
-                        $("#movieRating").text("Category: "+response.genres[0].name+ ".       Rating: "+response.popularity);
-                        openModel();                    
+                        if(Array.isArray(response.genres) && response.genres.length && response.popularity){
+                            // console.log("test");
+                            $("#movieRating").text("Category: "+response.genres[0].name+ ".       Rating: "+response.popularity);
+                        }
+                        
+                        // openModel();                    
 
+                        // document.ondblclick = function () {
+                        //     var sel = (document.selection && document.selection.createRange().text) ||
+                        //               (window.getSelection && window.getSelection().toString());
+                        //     alert(sel);
+                        //  };
+                         
                     })
                 }
             });
@@ -95,25 +126,9 @@ function appendImageToCarousel(url, hreF, counter) {
 $(document).ready(function () {
     $(".carousel").carousel();
     $(".carousel").empty();
-    loadInitialCarousel();
+    getMovieIDs();
+    // loadInitialCarousel();
     $('.modal').modal();// for the model
 
 });
 
-
-
-
-// https://api.themoviedb.org/3/movie/550?api_key=cc93940bfc0e8ed610b23598baac74fd
-
-// https://api.themoviedb.org/3/movie/343611?api_key=cc93940bfc0e8ed610b23598baac74fd
-
-// https://image.tmdb.org/t/p/w500/vUHqBgt5e7jMmMnvP1Dk1gpnvZO.jpg
-// https://image.tmdb.org/t/p/w500/nDS8rddEK74HfAwCC5CoT6Cwzlt.jpg
-// https://image.tmdb.org/t/p/w500/adw6Lq9FiC9zjYEpOqfq03ituwp.jpg
-
-// for multiple images of same movie
-// https://api.themoviedb.org/3/movie/550/images?api_key=cc93940bfc0e8ed610b23598baac74fd 
-
-// get id of movie and get images
-
-// <a class="carousel-item" href="#one!"><img src="https://picsum.photos/200/300?random=1"></a>
